7c8119f373b3407a6b590eb6c4504806887ec3a7
Alex Spaceteam <alexspaceteam@gmail.com>
2026-04-19 02:44:13 +0000

    test that plugin works

---
+++ src/main/java/com/baneodragons/BaneOfDragons.java
@@ -0,0 +1,27 @@
+package com.baneodragons;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BaneOfDragons extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("BaneOfDragons enabled!");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String name = event.getPlayer().getName();
+        getServer().broadcast(
+            Component.text("[BaneOfDragons] ", NamedTextColor.GOLD)
+                .append(Component.text(name, NamedTextColor.YELLOW))
+                .append(Component.text(" has joined the realm!", NamedTextColor.WHITE))
+        );
+    }
+}
